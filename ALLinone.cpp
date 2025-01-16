1db23b1bd843bc69c43c661e0171bf2cefb0bf8d
king kunal <105977628+kunal596@users.noreply.github.com>
2025-01-16 22:41:09 +0530

    everything is only in function recursion

--- ALLinone.cpp
+++ ALLinone.cpp
@@ -1,36 +1,90 @@
 #include <bits/stdc++.h>
 using namespace std;
-int b = 0;
-int rev(int n)
+class rew
 {
-    if (n == 0 && n < 10)
-        return n;
-    b = b * 10 + n % 10;
-    return rev(n / 10);
-}
-void output()
-{
-    int n;
-    cout << "enter a number: " << endl;
-    cin >> n;
-    rev(n);
-    if (n >= 10)
+public:
+    int b=0;
+    int rev(int n)
     {
-        cout << b << endl;
+        if (n == 0 && n < 10)
+            return n;
+        b = b * 10 + n % 10;
+        return rev(n / 10);
     }
-    else
-        return;
-}
+
+    int revoutput(int n)
+    {   
+        rev(n);
+        if (n >= 10)
+        {
+            cout << b << endl;
+        }
+        return  b=0;
+    }
+};
+
+class prm
+{
+public:
+    int b=0, a=0;
+    int prime(int n)
+    {
+        if(n==a)
+            return n;
+        if(n>a)
+            a++;
+        if(n%a==0)
+            b++;
+        return prime(n);
+    }
+
+    int primeoutput(int n)
+    { 
+        prime(n);
+        if (b==2)
+            cout <<"its prime: " << endl;
+        else
+            cout<<"its not prime"<<endl;
+        return a=0,b=0;
+    }
+};
+
+class fac
+{
+public:
+    int b=0;
+    int fact(int n)
+    {
+        if(n<=1)  // n==0 then it should return 1
+            return n; // if n==0 then n=1
+        return n*fact(n-1);
+    }
+
+    void factoutput(int n)
+    {   
+        cout<<fact(n)<<endl;
+    }
+};
+
+
 
 int main()
 {
     // driver code for multiple test cases
-    int n;
+    rew o;
+    prm o1;
+    fac o2;
+    int a,n;
     cout << "enter the test cases: " << endl;
-    cin >> n;
-    while (n--)
+    cin >> a;
+    while (a--)
     {
-        output();
+        cout << "enter a number: " << endl;
+        cin >> n;
+        // o.revoutput(n);
+        // o1.primeoutput(n);
+        o2.factoutput(n);
+
     }
     return 0;
 }
