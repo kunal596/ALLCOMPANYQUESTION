277c1b843a017b3dd6ff05abf91f978c57b28784
king kunal <105977628+kunal596@users.noreply.github.com>
2025-01-16 21:54:45 +0530

    reverser a number by using recursion

--- ALLinone.cpp
+++ ALLinone.cpp
@@ -1,36 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-class king
+int b = 0;
+int rev(int n)
 {
-public:
-    int a = 0, b = 1, c;
-    int fibbo(int n)
-    {
-        if (n <= 1)
-            return n;
-        return fibbo(n - 1) + fibbo(n - 2);
-    }
-
-    void fibo(int n) 
-    {
-        int a = 0, b = 1, c;
-        for (int i = 0; i < n - 1; i++)
-        {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        cout << c << endl;
-    }
-};
-
-int main()
+    if (n == 0 && n < 10)
+        return n;
+    b =  b * 10 + n % 10; 
+    return rev(n / 10);
+}
+void input()
 {
-    king o;
     int n;
     cout << "enter a number: " << endl;
     cin >> n;
-    cout << o.fibbo(n) << endl;
-    o.fibo(n);
+    rev(n);
+    if(n>=10){
+        cout<<b<<endl;
+    }
+    else 
+        return;
+}
+
+int main()
+{
+    int b;
+    input();
     return 0;
 }
