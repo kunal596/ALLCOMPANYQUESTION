e07bd4431b6e507b487d79b14e79119320c25633
king kunal <105977628+kunal596@users.noreply.github.com>
2025-01-16 22:43:25 +0530

    good

--- ALLinone.cpp
+++ ALLinone.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class rew
 {
 public:
-    int b=0;
+    int b = 0;
     int rev(int n)
     {
         if (n == 0 && n < 10)
@@ -13,60 +13,58 @@ public:
     }
 
     int revoutput(int n)
-    {   
+    {
         rev(n);
         if (n >= 10)
         {
             cout << b << endl;
         }
-        return  b=0;
+        return b = 0;
     }
 };
 
 class prm
 {
 public:
-    int b=0, a=0;
+    int b = 0, a = 0;
     int prime(int n)
     {
-        if(n==a)
+        if (n == a)
             return n;
-        if(n>a)
+        if (n > a)
             a++;
-        if(n%a==0)
+        if (n % a == 0)
             b++;
         return prime(n);
     }
 
     int primeoutput(int n)
-    { 
+    {
         prime(n);
-        if (b==2)
-            cout <<"its prime: " << endl;
+        if (b == 2)
+            cout << "its prime: " << endl;
         else
-            cout<<"its not prime"<<endl;
-        return a=0,b=0;
+            cout << "its not prime" << endl;
+        return a = 0, b = 0;
     }
 };
 
 class fac
 {
 public:
-    int b=0;
+    int b = 0;
     int fact(int n)
     {
-        if(n<=1)  // n==0 then it should return 1
+        if (n <= 1)   // n==0 then it should return 1
             return n; // if n==0 then n=1
-        return n*fact(n-1);
+        return n * fact(n - 1);
     }
 
     void factoutput(int n)
-    {   
-        cout<<fact(n)<<endl;
+    {
+        cout << fact(n) << endl;
     }
 };
-
-
 
 int main()
 {
@@ -74,7 +72,7 @@ int main()
     rew o;
     prm o1;
     fac o2;
-    int a,n;
+    int a, n;
     cout << "enter the test cases: " << endl;
     cin >> a;
     while (a--)
@@ -84,7 +82,6 @@ int main()
         // o.revoutput(n);
         // o1.primeoutput(n);
         o2.factoutput(n);
-
     }
     return 0;
 }
